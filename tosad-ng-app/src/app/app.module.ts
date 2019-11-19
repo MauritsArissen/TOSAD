0bdc5d1038a71f3a33dc52540d5be9e6dbf21450
Cor de Kruijf <cordekruijf@hotmail.nl>
2019-11-19 21:49:35 +0100

    Integrated Bootstrap 4

--- tosad-ng-app/src/app/app.module.ts
+++ tosad-ng-app/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
