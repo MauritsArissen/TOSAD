6eb29a66bd9b40957124c7b4bb68c89951e71a01
Maurits <maurits.arissen@student.hu.nl>
2019-11-19 14:14:13 +0100

    Initial commit

---
+++ tosad-api/src/main/java/nl/hu/v2a/team1/tosad/TosadApplication.java
@@ -0,0 +1,13 @@
+package nl.hu.v2a.team1.tosad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TosadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TosadApplication.class, args);
+	}
+
+}
