6fdc29e4b55d28610e23c513c05aec73be9444da
Maurits <maurits.arissen@student.hu.nl>
2020-01-13 16:35:15 +0100

    Layout

--- tosad-ng-app/src/app/app.module.ts
+++ tosad-ng-app/src/app/app.module.ts
@@ -5,11 +5,15 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    SidebarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
