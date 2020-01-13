8979ea59a9af357ae7e4b92dd9eb0e2545962d84
Maurits <maurits.arissen@student.hu.nl>
2020-01-13 11:10:49 +0100

    Clean project

--- tosad-ng-app/src/app/app-routing.module.ts
+++ tosad-ng-app/src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
 
-
-const routes: Routes = [
-  {path: '', component: HeaderComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- tosad-ng-app/src/app/app.module.ts
+++ tosad-ng-app/src/app/app.module.ts
@@ -4,14 +4,10 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    MainComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
