3c73a5b5cc43236216270bff4f25830f970a386a
ehoudijk <eric.houdijk@student.hu.nl>
2020-01-16 18:42:17 +0100

    postfunctie trial

--- tosad-api/src/main/java/define/resource/DefineResource.java
+++ tosad-api/src/main/java/define/resource/DefineResource.java
@@ -1,10 +1,8 @@
 package define.resource;
 
 import javax.annotation.PostConstruct;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/define")
@@ -24,8 +22,15 @@ public class DefineResource {
         }
     }
 
-//    @POST
-//    @Path("/savedefined")
-//    public Response
+    @POST
+    @Path("/savedefined")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response doSomethingWithUiInput(postJsonUIinput in) {
+	    System.out.println("test");
+	    System.out.println("tablename: " + in.getTable());
+
+	    return Response.ok().build();
+    }
 
 }
