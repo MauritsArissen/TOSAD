4c8d10fc89b59510c73e1aa7f5464d7346dfb0dc
Redouanelh <redouan_school@outlook.com>
2020-01-16 11:55:08 +0100

    jersy added few

--- tosad-api/src/main/java/define/resource/DefineResource.java
+++ tosad-api/src/main/java/define/resource/DefineResource.java
@@ -1,6 +1,21 @@
 package define.resource;
 
+import com.sun.research.ws.wadl.Response;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/define")
 public class DefineResource {
+
+    @GET
+    @Produces("application/json")
+    public Response getBasicInfo() {
+
+
+
+    }
 
 
 
