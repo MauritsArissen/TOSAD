d5db505f4457f967f3195ad6a8a75cdc86e4bae7
Redouanelh <redouan_school@outlook.com>
2020-01-16 16:10:54 +0100

    JERSEY WORKS

--- tosad-api/src/main/java/Main.java
+++ tosad-api/src/main/java/Main.java
@@ -1,30 +1,9 @@
-import define.persistence.OracleBaseDao;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         System.out.println("Main class works");
 
-        String query = "select * from businessruletype where code = 'ACMP'";
-        String result = "";
-
-        Connection conn = new OracleBaseDao().getConnection();
-
-        PreparedStatement pstmt = conn.prepareStatement(query);
-        ResultSet myRs = pstmt.executeQuery();
-
-        while (myRs.next()) {
-            result += myRs.getString("code");
         }
-
-        myRs.close();
-        pstmt.close();
-
-        }
+    
     }
 
--- tosad-api/src/main/java/define/resource/DefineResource.java
+++ tosad-api/src/main/java/define/resource/DefineResource.java
@@ -3,18 +3,19 @@ package define.resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 @Path("/define")
 public class DefineResource {
-
-    @GET
+	
+	@GET
     @Produces("application/json")
-    public String getBasicInfo() {
+    public Response getBasicInfo() {
         System.out.println("test");
         try {
             String output = "Hello world!";
             System.out.println(output);
-            return output;
+            return Response.ok().build();
         } catch (Exception e) {
             System.out.println("test2");
             return null;
