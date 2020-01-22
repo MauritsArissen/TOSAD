c258a3279fb15c4893932a568e9f768ba4c12c43
Maurits <maurits.arissen@student.hu.nl>
2020-01-22 15:02:41 +0100

    Push

--- tosad-ng-app/src/app/http.service.ts
+++ tosad-ng-app/src/app/http.service.ts
@@ -16,10 +16,14 @@ export class HttpService {
     return this.http.get(url);
   }
 
-  postRequest(url, body, options) {
+  postRequest(url, body) {
     let httpHeaders = new HttpHeaders({
       'Content-Type' : 'application/json',
       'Cache-Control': 'no-cache',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "origin, content-type, accept, authorization",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS, HEAD"
     });   
     return this.http.post(url, body, { headers: httpHeaders })
   }
