700395d9c1d4ead50e8b80520d7d07ff1a543b81
Cor de Kruijf <cordekruijf@hotmail.nl>
2020-01-22 16:07:55 +0100

    Add trigger list (dummy data)

--- tosad-ng-app/src/app/manage/manage.component.ts
+++ tosad-ng-app/src/app/manage/manage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-manage',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _http: HttpService) { }
 
   ngOnInit() {
   }
 
+  // Example data
+  triggers: { [key: string]: Object } = {
+    "trigger t1": {
+      "rules": "attribute a1"
+    },
+    "trigger t2": {
+      "rules": "attribute a2"
+    }
+  }
+
+  rules = []
+
+  getRules(triggerName) {
+    this.rules = [
+      {
+        "name": this.triggers[triggerName]["rules"]
+      }
+    ]
+  }
+    
 }
