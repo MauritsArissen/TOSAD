bfbb4a68df49e5214acd33c09b66d96af9de49e8
ehoudijk <eric.houdijk@student.hu.nl>
2020-01-22 10:55:03 +0100

    Attribute rule classes

--- tosad-api/src/main/java/generate/business/controller/factory/BusinessRuleFactory.java
+++ tosad-api/src/main/java/generate/business/controller/factory/BusinessRuleFactory.java
@@ -1,10 +1,6 @@
 package generate.business.controller.factory;
 
 import generate.business.domain.*;
-import generate.business.domain.businessrules.BusinessRule;
-import generate.business.domain.businessrules.RangeRule;
-
-import java.util.ArrayList;
 
 public interface BusinessRuleFactory {
     String createRule(String name, Operator operator, Trigger trigger, Table table);
