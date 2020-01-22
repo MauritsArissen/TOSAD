3ae69cfede268063d7155ac920f745d2d21f8077
Redouanelh <redouan_school@outlook.com>
2020-01-22 13:32:03 +0100

    fetch all table and columns wioth categories etc works

--- tosad-api/src/main/java/define/business/controller/DefineController.java
+++ tosad-api/src/main/java/define/business/controller/DefineController.java
@@ -3,20 +3,37 @@ package define.business.controller;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import define.persistence.adapter.DaoAdapter;
+import define.persistence.dao.BaseDao;
 import define.persistence.dao.DefineOracleDao;
+import define.persistence.dao.TargetOracleDao;
 
 public class DefineController {
 
     public DefineController() {}
 
-    public HashMap<String, HashMap<String, HashMap<String, HashMap<String, ArrayList<String>>>>> getBusinessRuleData() {
+	public HashMap<String, HashMap> getDefineData() {
         try {
-        	// returns all available user from the database as a hashmap
-            return new DefineOracleDao().getAvailableInput();
+        	BaseDao defineconnectionadapter = new DaoAdapter().serialize("Oracle", "jdbc:oracle:thin:@//ondora04.hu.nl:1521/EDUC11", "cursist", "cursist8101");
+        	HashMap<String, HashMap<String, HashMap<String, HashMap<String, ArrayList<String>>>>> ruledata = new HashMap<>();
+        	ruledata = new DefineOracleDao(defineconnectionadapter).getAvailableInput();
+        
+        	BaseDao targetconnectionadapter = new DaoAdapter().serialize("Oracle", "jdbc:oracle:thin:@//ondora04.hu.nl:1521/EDUC11", "maurits", "maurits");
+        	HashMap<String, HashMap<String, HashMap<String, HashMap<String, String>>>> targetdata = new HashMap<>();
+        	targetdata = new TargetOracleDao(targetconnectionadapter).loadTargetDatabase();
+        	
+			HashMap<String, HashMap> totaldata= new HashMap();
+        	totaldata.put("categories", ruledata.get("categories"));
+        	totaldata.put("datatable", targetdata.get("datatable"));
+        	
+        	return totaldata;
         } catch (Exception e) {
             System.out.println(e);
         }
+        System.out.println("Failed to execute method.");
         return null;
     }
+	
+	
 
 }
--- tosad-api/src/main/java/define/persistence/adapter/GenerateSerializer.java
+++
@@ -1,8 +0,0 @@
-package define.persistence.adapter;
-
-import define.business.domain.businessrules.BusinessRule;
-
-public interface GenerateSerializer {
-
-    public String serialize(BusinessRule rule);
-}
