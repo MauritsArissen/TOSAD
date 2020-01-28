41d10d2dd425a1a59a956caa3be32a6bb3be8071
RGadella <43090705+RGadella@users.noreply.github.com>
2020-01-28 11:11:17 +0100

    Attribute Compare also works with numbers

--- tosad-api/src/main/java/generate/business/domain/businessrules/AttributeCompareRule.java
+++ tosad-api/src/main/java/generate/business/domain/businessrules/AttributeCompareRule.java
@@ -3,6 +3,7 @@ package generate.business.domain.businessrules;
 import generate.business.domain.businessrules.ruleattributes.LiteralValue;
 import generate.business.domain.businessrules.ruleattributes.Operator;
 import generate.business.domain.businessrules.ruleattributes.Table;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.util.ArrayList;
 
@@ -39,9 +40,13 @@ public class AttributeCompareRule implements BusinessRule {
 
     public String generateDynamicPart() {
         String template =  "--" + name + "\n" +
-                "l_passed := :new." + table.getSelectedTableAttribute().getName() + " " + operator.getName() + " '" +
-                values.get(0).getValue() + "';\n" +
-                "  if not l_passed\n" +
+                "l_passed := :new." + table.getSelectedTableAttribute().getName() + " " + operator.getName() + " ";
+        if(NumberUtils.isNumber(values.get(0).getValue())) {
+            template += values.get(0).getValue() + ";\n";
+        } else {
+            template += "'" + values.get(0).getValue() + "';\n";
+        }
+        template += "  if not l_passed\n" +
                 "  then\n" +
                 "    l_error_stack := '" + failuremessage + "';\n" +
                 "    raise_application_error( -20800, l_error_stack );\n" +
