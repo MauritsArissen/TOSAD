d6867bfa4ea8cec23c32aaafa5ddd8f79e2f8ad1
Redouanelh <redouan_school@outlook.com>
2020-01-29 10:28:12 +0100

    dfdfd

--- tosad-api/src/main/java/define/resource/DefineResource.java
+++ tosad-api/src/main/java/define/resource/DefineResource.java
@@ -73,5 +73,27 @@ public class DefineResource {
         return builder.build(); 
 	}
 	
+	@POST
+	@Path("/login") 
+	public Response login(String data) {
+		
+		System.out.println(data);
+		
+//		String response = controller.login(data);
+		HashMap<String, String> result = new HashMap();
+//		result.put("response", response);
+		ResponseBuilder builder = Response.ok(result);
+	       
+	    builder.header("Access-Control-Allow-Origin", "*");
+        builder.header("Access-Control-Allow-Headers",
+                "origin, content-type, accept, authorization");
+        builder.header("Access-Control-Allow-Credentials", "true");
+        builder.header("Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        
+        return builder.build(); 
+	}
+	
+	
 
 }
