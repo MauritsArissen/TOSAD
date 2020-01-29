9e0d1b7951f9ca72d967e95c6ad0ddfcca46816e
DamiandeRijk <damian04@msn.com>
2020-01-29 13:51:33 +0100

    targetdao factory in generate

--- tosad-api/src/main/java/generate/persistence/dao/DefineDao.java
+++ tosad-api/src/main/java/generate/persistence/dao/DefineDao.java
@@ -1,8 +1,13 @@
 package generate.persistence.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public interface DefineDao {
     ArrayList<String> getTriggerInfo();
     ArrayList<String> getRulesByTrigger(String triggername);
+
+    ArrayList<HashMap<String, String>> getAllDataFromTrigger(String triggercode);
+
+    ArrayList<String> getValuesFromRule(String name);
 }
--- tosad-api/src/main/java/generate/persistence/dao/TargetDao.java
+++ tosad-api/src/main/java/generate/persistence/dao/TargetDao.java
@@ -1,4 +1,7 @@
 package generate.persistence.dao;
 
+import java.util.ArrayList;
+
 public interface TargetDao {
+    ArrayList<String> executeCode(String s);
 }
