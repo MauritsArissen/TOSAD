9f247099e38a0a6241ee0362b22c706b24a77646
Maurits <maurits.arissen@student.hu.nl>
2020-01-29 10:35:50 +0100

    Login screen errors

--- tosad-ng-app/src/app/login/login.component.ts
+++ tosad-ng-app/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 declare const M;
 
@@ -10,9 +11,15 @@ declare const M;
 export class LoginComponent implements OnInit, AfterViewInit {
 
   dbTypes: string[] = ["Oracle"];
-  dbType: string = "Select database type";
+  dbType: string = "Oracle";
+  host: string = "ondora04.hu.nl";
+  port: number = 1521;
+  SID: string = "";
+  ServiceName: string = "EDUC10";
+  username: string = "maurits";
+  password: string = "maurits";
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
@@ -25,6 +32,23 @@ export class LoginComponent implements OnInit, AfterViewInit {
     setTimeout(() => {
       M.AutoInit()
     }, 10)
+  }
+
+  connect() {
+    if (this.host.length == 0) {
+      return M.toast({html: "A host is required to connect", classes: "errorToast"})
+    } else if (this.port == (null || undefined || 0) || this.port < 1 || this.port > 65565) {
+      return M.toast({html: "You must choose a port from 1-65565", classes: "errorToast"})
+    } else if (this.SID == "" && this.ServiceName == "") {
+      return M.toast({html: "You must fill in either SID or Service name", classes: "errorToast"})
+    } else if (this.username == "") {
+      return M.toast({html: "You must fill in a username", classes: "errorToast"})
+    } else if (this.password == "") {
+      return M.toast({html: "You must fill in a password", classes: "errorToast"})
+    } else if (this.dbType == "") {
+      return M.toast({html: "You must select one of the supported database types", classes: "errorToast"})
+    }
+    this.router.navigate(['/define'])
   }
 
 }
