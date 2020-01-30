0f467b0967884e396f372f3b9bfbd6948875f21e
Maurits <maurits.arissen@student.hu.nl>
2020-01-30 10:34:33 +0100

    Update TypeBasedBusinessRuleFactory.java

--- tosad-api/src/main/java/generate/business/domain/factory/TypeBasedBusinessRuleFactory.java
+++ tosad-api/src/main/java/generate/business/domain/factory/TypeBasedBusinessRuleFactory.java
@@ -12,25 +12,25 @@ public class TypeBasedBusinessRuleFactory implements BusinessRuleFactory {
     }
 
     @Override
-    public BusinessRule createRule(Operator operator, Table table, String failuremessage, String name) {
+    public BusinessRule createRule(Operator operator, Table table, String failuremessage, String name, String constraintTemplate, String declareTemplate) {
         if (type.equals("Attribute Range rule")) {
-            return new AttributeRangeRule(operator, table, failuremessage, name);
+            return new AttributeRangeRule(operator, table, failuremessage, name, "", "");
         } else if (type.equals("Attribute Compare rule")) {
-            return new AttributeCompareRule(operator, table, failuremessage, name);
+            return new AttributeCompareRule(operator, table, failuremessage, name, "", "");
         } else if (type.equals("Attribute List rule")) {
-            return new AttributeListRule(operator, table, failuremessage, name);
+            return new AttributeListRule(operator, table, failuremessage, name, "", "");
         } else if (type.equals("Attribute Other rule")) {
-            return new AttributeOtherRule(operator, table, failuremessage, name);
+            return new AttributeOtherRule(operator, table, failuremessage, name, "", "");
         } else if (type.equals("Tuple Compare rule")) {
-            return new TupleCompareRule(operator, table, failuremessage, name);
+            return new TupleCompareRule(operator, table, failuremessage, name, "", "");
         } else if (type.equals("Tuple Other rule")) {
-            return new TupleOtherRule(operator, table, failuremessage, name);
+            return new TupleOtherRule(operator, table, failuremessage, name, "", "");
         } else if (type.equals("Inter-Entity Compare rule")) {
-            return new InterEntityCompareRule(operator, table, failuremessage, name);
+            return new InterEntityCompareRule(operator, table, failuremessage, name, "", "");
         } else if (type.equals("Entity Other rule")) {
-            return new EntityOtherRule(operator, table, failuremessage, name);
+            return new EntityOtherRule(operator, table, failuremessage, name, "", "");
         } else {
-            return new ModifyRule(operator, table, failuremessage, name);
+            return new ModifyRule(operator, table, failuremessage, name, "", "");
         }
     }
 }
