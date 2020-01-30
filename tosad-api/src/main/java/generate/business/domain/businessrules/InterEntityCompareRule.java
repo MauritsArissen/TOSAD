543e709c73f6e91b313a16af5b88f5b16d1afc6b
DamiandeRijk <damian04@msn.com>
2020-01-30 16:09:26 +0100

    last push?

--- tosad-api/src/main/java/generate/business/domain/businessrules/InterEntityCompareRule.java
+++ tosad-api/src/main/java/generate/business/domain/businessrules/InterEntityCompareRule.java
@@ -37,26 +37,41 @@ public class InterEntityCompareRule implements BusinessRule {
     }
 
     public String generateDynamicPart() {
-        String template = "--" + name + " code\n" +
-                "open lc_first;\n" +
-                "fetch lc_first into l_targetValue;\n" +
-                "close lc_first;\n" +
-                "l_passed := :new." + table.getSelectedTableAttribute().getName() + " " + operator.getName() + " l_targetValue;\n" +
-                "  if not l_passed\n" +
-                "  then\n" +
-                "    l_error_stack := '" + failuremessage + "';\n" +
-                "    raise_application_error( -20800, l_error_stack );\n" +
-                "  end if;\n";
+        String template =  "--" + name + " declare\n";
+        constraintTemplate = constraintTemplate.replace("[templateid]", name.split("_")[5]);
+        constraintTemplate = constraintTemplate.replace("[targettable]", values.get(0).getValue());
+        constraintTemplate = constraintTemplate.replace("[targettableattribute]", values.get(1).getValue());
+        constraintTemplate = constraintTemplate.replace("[targetkey]", values.get(3).getValue());
+        constraintTemplate = constraintTemplate.replace("[selectedkey]", values.get(2).getValue());
+        constraintTemplate = constraintTemplate.replace("[selectedtableattribute]", table.getSelectedTableAttribute().getName());
+        constraintTemplate = constraintTemplate.replace("[operator]", operator.getName());
+        template += constraintTemplate + "\n";
+
+//        String template = "--" + name + " code\n" +
+//                "open lc_first;\n" +
+//                "fetch lc_first into l_targetValue;\n" +
+//                "close lc_first;\n" +
+//                "l_passed := :new." + table.getSelectedTableAttribute().getName() + " " + operator.getName() + " l_targetValue;\n" +
+//                "  if not l_passed\n" +
+//                "  then\n" +
+//                "    l_error_stack := '" + failuremessage + "';\n" +
+//                "    raise_application_error( -20800, l_error_stack );\n" +
+//                "  end if;\n";
         return template;
     }
 
     public String generateDeclare() {
-        String template =  "--" + name + " declare\n" +
-                "cursor lc_first is\n" +
-                "select " + values.get(1).getValue() + "\n" +
-                "from " + values.get(0).getValue()  + "\n" +
-                "l_targetValue " + values.get(0).getValue() + "." + values.get(1).getValue() + "%type;\n";
-
+        String template =  "--" + name + " declare\n";
+        declareTemplate = declareTemplate.replace("[templateid]", name.split("_")[5]);
+        declareTemplate = declareTemplate.replace("[targettable]", values.get(0).getValue());
+        declareTemplate = declareTemplate.replace("[targettableattribute]", values.get(1).getValue());
+        template += declareTemplate + "\n";
+        System.out.println(template);
+//        String template =  "--" + name + " declare\n" +
+//                "cursor lc_first is\n" +
+//                "select " + values.get(1).getValue() + "\n" +
+//                "from " + values.get(0).getValue()  + "\n" +
+//                "l_targetValue " + values.get(0).getValue() + "." + values.get(1).getValue() + "%type;\n";
         return template;
     }
 
